1d11d47bcf59e700888e13f1b5ed53fb31e15eb9
Mohammad Reza <rza9001@gmail.com>
2018-05-02 20:52:18 +0700

    Added 05_Main_Area_4_Counter

---
+++ 01_Ca/05_Main_Area_4_Counter/main.js
@@ -0,0 +1,7 @@
+// CounterUp Active Code (number animation), needs waypoint.js
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
